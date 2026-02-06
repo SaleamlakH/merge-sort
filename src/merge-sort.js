fb9244122df4303bfa22087572c8322e71c8ee8d
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-06 12:15:09 +0300

    Feat: Implement merge sort algorithm with tests

--- src/merge-sort.js
+++ src/merge-sort.js
@@ -1,1 +1,33 @@
-export const check = "It's ready";
+export const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  // sort the left half
+  const mid = arr.length / 2;
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  // merge left and right
+  return merge(left, right);
+};
+
+const merge = (left, right) => {
+  const sorted = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] <= right[rightIndex]) {
+      sorted.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sorted.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  // anything left in either of the arrays
+  // can be simply pushed into the sorted array
+  sorted.push(...left.slice(leftIndex))
+  sorted.push(...right.slice(rightIndex));
+
+  return sorted;
+};
--- tests/merge-sort.test.js
+++ tests/merge-sort.test.js
@@ -1,3 +1,27 @@
-import { check } from 'merge-sort';
+import { mergeSort } from "merge-sort";
 
-console.log(check);
+describe("merge-sort", () => {
+  test("sort a single number array", () => {
+    expect(mergeSort([1])).toEqual([1]);
+  });
+
+  test("sort empty array", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+
+  test('sort 2 numbers array', () => {
+    expect(mergeSort([2, 1])).toEqual([1, 2]);
+  })
+
+  test('sort 5 numbers array', () => {
+    expect(mergeSort([1, 3, 4, 2, 5])).toEqual([1, 2, 3, 4, 5])
+  })
+
+  test('sort already sorted 5 numbers array', () => {
+    expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  })
+
+  test('sort 8 numbers array', () => {
+    expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  })
+});
